cb66d38a69c8ab51b85ba1aedb2e73ec0bc96b98
林恒 <hunt.lin@outlook.com>
2022-12-29 20:46:35 +0800

    tescis

--- hidtest/test.c
+++ hidtest/test.c
@@ -460,6 +460,166 @@ int test_huion(void)
 	return 0;
 }
 
+// vid: 0x0ed1
+// pid: 0x04f8
+int test_tescis(void)
+{
+	int res;
+	unsigned char buf[256];
+#define MAX_STR 255
+	wchar_t wstr[MAX_STR];
+	hid_device* handle;
+	int i;
+
+	// Set up the command buffer.
+	memset(buf, 0x00, sizeof(buf));
+	buf[0] = 0x01;
+	buf[1] = 0x81;
+
+	// Open the device using the VID, PID,
+	// and optionally the Serial number.
+	////handle = hid_open(0x4d8, 0x3f, L"12345");
+	handle = hid_open(0x0ed1, 0x04f8, NULL);
+	if (!handle) {
+		printf("unable to open device\n");
+		hid_exit();
+		return 1;
+	}
+
+	// Read the Manufacturer String
+	wstr[0] = 0x0000;
+	res = hid_get_manufacturer_string(handle, wstr, MAX_STR);
+	if (res < 0)
+		printf("Unable to read manufacturer string\n");
+	printf("Manufacturer String: %ls\n", wstr);
+
+	// Read the Product String
+	wstr[0] = 0x0000;
+	res = hid_get_product_string(handle, wstr, MAX_STR);
+	if (res < 0)
+		printf("Unable to read product string\n");
+	printf("Product String: %ls\n", wstr);
+
+	// Read the Serial Number String
+	wstr[0] = 0x0000;
+	res = hid_get_serial_number_string(handle, wstr, MAX_STR);
+	if (res < 0)
+		printf("Unable to read serial number string\n");
+	printf("Serial Number String: (%d) %ls", wstr[0], wstr);
+	printf("\n");
+
+	struct hid_device_info* info = hid_get_device_info(handle);
+	if (info == NULL) {
+		printf("Unable to get device info\n");
+	}
+	else {
+		print_devices(info);
+	}
+
+	//// Read Indexed String 1
+	//wstr[0] = 0x0000;
+	//res = hid_get_indexed_string(handle, 201, wstr, MAX_STR);
+	//if (res < 0)
+	//	printf("Unable to read indexed string 1\n");
+	//printf("Indexed String 1: %ls\n", wstr);
+
+	// Set the hid_read() function to be non-blocking.
+	hid_set_nonblocking(handle, 1);
+
+	hid_close(handle);
+
+	return 0;
+
+	// Try to read from the device. There should be no
+	// data here, but execution should not block.
+	res = hid_read(handle, buf, 17);
+
+	// Send a Feature Report to the device
+	buf[0] = 0x2;
+	buf[1] = 0xa0;
+	buf[2] = 0x0a;
+	buf[3] = 0x00;
+	buf[4] = 0x00;
+	res = hid_send_feature_report(handle, buf, 17);
+	if (res < 0) {
+		printf("Unable to send a feature report.\n");
+	}
+
+	memset(buf, 0, sizeof(buf));
+
+	// Read a Feature Report from the device
+	buf[0] = 0x2;
+	res = hid_get_feature_report(handle, buf, sizeof(buf));
+	if (res < 0) {
+		printf("Unable to get a feature report: %ls\n", hid_error(handle));
+	}
+	else {
+		// Print out the returned buffer.
+		printf("Feature Report\n   ");
+		for (i = 0; i < res; i++)
+			printf("%02x ", (unsigned int)buf[i]);
+		printf("\n");
+	}
+
+	memset(buf, 0, sizeof(buf));
+
+	// Toggle LED (cmd 0x80). The first byte is the report number (0x1).
+	buf[0] = 0x1;
+	buf[1] = 0x80;
+	res = hid_write(handle, buf, 17);
+	if (res < 0) {
+		printf("Unable to write(): %ls\n", hid_error(handle));
+	}
+
+	// Request state (cmd 0x81). The first byte is the report number (0x1).
+	buf[0] = 0x1;
+	buf[1] = 0x81;
+	hid_write(handle, buf, 17);
+	if (res < 0) {
+		printf("Unable to write()/2: %ls\n", hid_error(handle));
+	}
+
+	// Read requested state. hid_read() has been set to be
+	// non-blocking by the call to hid_set_nonblocking() above.
+	// This loop demonstrates the non-blocking nature of hid_read().
+	res = 0;
+	i = 0;
+	while (res == 0) {
+		res = hid_read(handle, buf, sizeof(buf));
+		if (res == 0) {
+			printf("waiting...\n");
+		}
+		if (res < 0) {
+			printf("Unable to read(): %ls\n", hid_error(handle));
+			break;
+		}
+
+		i++;
+		if (i >= 10) { /* 10 tries by 500 ms - 5 seconds of waiting*/
+			printf("read() timeout\n");
+			break;
+		}
+
+#ifdef _WIN32
+		Sleep(500);
+#else
+		usleep(500 * 1000);
+#endif
+	}
+
+	if (res > 0) {
+		printf("Data read:\n   ");
+		// Print out the returned buffer.
+		for (i = 0; i < res; i++)
+			printf("%02x ", (unsigned int)buf[i]);
+		printf("\n");
+	}
+
+	hid_close(handle);
+
+	return 0;
+}
+
 // 0x0EEF - VID
 // 0xC121 - PID
 int test_eeti(void)
@@ -731,8 +891,6 @@ int main(int argc, char* argv[])
 	(void)argc;
 	(void)argv;
 
-	//struct hid_device_info *devs;
-
 	printf("hidapi test/example tool. Compiled with hidapi version %s, runtime version %s.\n", HID_API_VERSION_STR, hid_version_str());
 	if (HID_API_VERSION == HID_API_MAKE_VERSION(hid_version()->major, hid_version()->minor, hid_version()->patch)) {
 		printf("Compile-time version matches runtime version of hidapi.\n\n");
@@ -750,11 +908,12 @@ int main(int argc, char* argv[])
 	hid_darwin_set_open_exclusive(0);
 #endif
 
-	//devs = hid_enumerate(0x0, 0x0);
-	//print_devices(devs);
-	//hid_free_enumeration(devs);
+	struct hid_device_info *devs;
+	devs = hid_enumerate(0x0, 0x0);
+	print_devices(devs);
+	hid_free_enumeration(devs);
 
-	if (test_huion()) {
+	if (test_tescis()) {
 		return 1;
 	}
 
